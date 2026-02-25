a3994be64e281a31f6568318effe589e96204c26
sangram-jr <sangrambera58@gmail.com>
2026-02-25 11:31:48 +0530

    add broadcast logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,27 @@
-import {WebSocketServer} from "ws"
+import {WebSocketServer,WebSocket} from "ws"
 const wss=new WebSocketServer({port:8080});
 
+let userCount=0;
+const totalSocket:WebSocket[] = [];
+
 wss.on('connection',(socket)=>{
-    console.log("User Connected");
+    totalSocket.push(socket);
+    userCount+=1;
+    console.log("User Connected: " + userCount);
 
     socket.on('message',(msg)=>{ //to send the message to the server
         console.log(msg.toString());
-        //server sent back message to the user
-        socket.send(msg.toString());
+
+        //server sent back message to the user(client)
+        //every user can see the msg
+        for(let i=0;i<totalSocket.length;i++){
+            const s=totalSocket[i];
+            if(s){
+                s.send(msg.toString() + ": send from server");
+            }
+            
+        }
+        
     })
     
 })
